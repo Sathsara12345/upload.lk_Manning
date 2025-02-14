a0ed67a1e900b8342ea3a3b22ba49d68917bf039
Nethma de silva <nethmavilhan@gmail.com>
2025-02-14 14:17:44 +0530

    Add all changes

--- database/migrations/2025_02_13_100134_create_warehouses_table.php
+++ database/migrations/2025_02_13_100134_create_warehouses_table.php
@@ -13,9 +13,16 @@ return new class extends Migration
     {
         Schema::create('warehouses', function (Blueprint $table) {
             $table->id();
+            $table->string('packs')->nullable(); // add packs column
+            $table->string('item_name')->nullable(); // add item_name column
+            $table->string('lorry_no')->nullable(); // add lorry_no column
+            $table->decimal('weight', 10, 2)->nullable(); // add weight column
+            $table->decimal('weight_balance', 10, 2)->nullable(); // add weight_balance column
+            $table->decimal('total_sales_as_per', 10, 2)->nullable(); // add total_sales_as_per column
             $table->timestamps();
         });
     }
+    
 
     /**
      * Reverse the migrations.
